7eabf0386287556ccd27bbd70cf9af91e47e2573
Stefan Johnson <sj@scrimpycat.io>
2017-06-01 14:15:47 +1000

    Create business table

---
+++ apps/gingerbread_house_service/priv/repo/migrations/20170601033001_business.exs
@@ -0,0 +1,37 @@
+defmodule GingerbreadHouse.Service.Repo.Migrations.Business do
+    use Ecto.Migration
+
+    def change do
+        create table(:businesses) do
+            add :entity, :uuid,
+                null: false
+
+            add :type, :string,
+                null: false
+
+            add :name, :string,
+                null: false
+
+            add :contact, :string,
+                null: false
+
+            add :country, :char,
+                size: 2,
+                null: false,
+                comment: "The ISO 3166-1 alpha-2 code for the country"
+
+            add :address, :string,
+                null: false
+
+            add :representative_name, :string,
+                null: false
+
+            add :representative_date_of_birth, :date,
+                null: false
+
+            timestamps()
+        end
+
+        create index(:businesses, [:entity], unique: true)
+    end
+end
