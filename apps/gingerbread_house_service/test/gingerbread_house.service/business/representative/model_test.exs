21e3eac49e8e1e2b5e6fa001c1cb10a26e4d558f
Stefan Johnson <sj@scrimpycat.io>
2017-06-02 21:55:20 +1000

    Test business representative model

---
+++ apps/gingerbread_house_service/test/gingerbread_house.service/business/representative/model_test.exs
@@ -0,0 +1,73 @@
+defmodule GingerbreadHouse.Service.Business.Representative.ModelTest do
+    use GingerbreadHouse.Service.Case
+
+    alias GingerbreadHouse.Service.Business.Representative
+
+    @valid_model %Representative.Model{
+        business_id: 1,
+        name: "test",
+        birth_date: ~D[2017-01-01],
+        address: "123 test st, foo",
+        owner: false
+    }
+
+    test "empty" do
+        refute_change(%Representative.Model{}, %{}, :insert_changeset)
+    end
+
+    test "only business" do
+        refute_change(%Representative.Model{}, %{ business_id: @valid_model.business_id }, :insert_changeset)
+
+        assert_change(@valid_model, %{ business_id: 0 }, :update_changeset)
+    end
+
+    test "only name" do
+        refute_change(%Representative.Model{}, %{ name: @valid_model.name }, :insert_changeset)
+
+        assert_change(@valid_model, %{ name: "foo" }, :update_changeset)
+    end
+
+    test "only birth date" do
+        refute_change(%Representative.Model{}, %{ birth_date: @valid_model.birth_date }, :insert_changeset)
+
+        assert_change(@valid_model, %{ birth_date: ~D[2017-01-01] }, :update_changeset)
+    end
+
+    test "only address" do
+        refute_change(%Representative.Model{}, %{ address: @valid_model.address }, :insert_changeset)
+
+        assert_change(@valid_model, %{ address: "+123 foo st, bar" }, :update_changeset)
+    end
+
+    test "only owner" do
+        refute_change(%Representative.Model{}, %{ owner: @valid_model.owner }, :insert_changeset)
+
+        assert_change(@valid_model, %{ owner: true }, :update_changeset)
+    end
+
+    test "without business" do
+        refute_change(@valid_model, %{ business_id: nil }, :insert_changeset)
+    end
+
+    test "without name" do
+        refute_change(@valid_model, %{ name: nil }, :insert_changeset)
+    end
+
+    test "without birth date" do
+        refute_change(@valid_model, %{ birth_date: nil }, :insert_changeset)
+    end
+
+    test "without address" do
+        refute_change(@valid_model, %{ address: nil }, :insert_changeset)
+    end
+
+    test "without owner" do
+        refute_change(@valid_model, %{ owner: nil }, :insert_changeset)
+    end
+
+    test "valid model" do
+        assert_change(@valid_model, %{}, :insert_changeset)
+
+        assert_change(@valid_model, %{}, :update_changeset)
+    end
+end
