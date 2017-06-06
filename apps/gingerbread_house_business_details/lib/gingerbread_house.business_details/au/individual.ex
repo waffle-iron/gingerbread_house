4359f1a49e968606138d22d55f534078f1bad8d5
Stefan Johnson <sj@scrimpycat.io>
2017-06-07 03:00:07 +1000

    Implement map conversion for individual AU business

--- apps/gingerbread_house_business_details/lib/gingerbread_house.business_details/au/individual.ex
+++ apps/gingerbread_house_business_details/lib/gingerbread_house.business_details/au/individual.ex
@@ -2,8 +2,66 @@ defmodule GingerbreadHouse.BusinessDetails.AU.Individual do
     defstruct [
         :name,
         :contact,
-        :street, :suburb, :postcode, :state,
+        :address,
         :abn,
         :bank
     ]
+
+    alias GingerbreadHouse.BusinessDetails.AU.Address
+    alias GingerbreadHouse.BusinessDetails.AU.Bank
+    alias GingerbreadHouse.BusinessDetails.AU.Individual
+
+    @behaviour GingerbreadHouse.BusinessDetails
+
+    def new(details) do
+        %Individual{}
+        |> new_name(details)
+        |> new_contact(details)
+        |> new_address(details)
+        |> new_abn(details)
+        |> new_bank(details)
+    end
+
+    defp new_name(info, %{ name: name }), do: %{ info | name: name }
+    defp new_name(info, _), do: info
+
+    defp new_contact(info, %{ contact: contact }), do: %{ info | contact: contact }
+    defp new_contact(info, _), do: info
+
+    defp new_address(info, %{ address: %{ "street" => street, "city" => city, "postcode" => postcode, "state" => state } }), do: %{ info | address: %Address{ street: street, city: city, postcode: postcode, state: state } }
+    defp new_address(info, _), do: info
+
+    defp new_abn(info, %{ additional_details: %{ "abn" => abn } }), do: %{ info | abn: abn }
+    defp new_abn(info, _), do: info
+
+    defp new_bank(info, %{ additional_details: %{ "bank" => %{ "bsb" => bsb, "account_number" => account_number } } }), do: %{ info | bank: %Bank{ bsb: bsb, account_number: account_number } }
+    defp new_bank(info, _), do: info
+
+    defimpl GingerbreadHouse.BusinessDetails.Mapper, for: Individual do
+        def to_map(info) do
+            %{ type: :individual, country: "AU" }
+            |> set_name(info)
+            |> set_contact(info)
+            |> set_address(info)
+            |> set_abn(info)
+            |> set_bank(info)
+        end
+
+        defp set_name(details, %{ name: nil }), do: details
+        defp set_name(details, %{ name: name }), do: Map.put(details, :name, name)
+
+        defp set_contact(details, %{ contact: nil }), do: details
+        defp set_contact(details, %{ contact: contact }), do: Map.put(details, :contact, contact)
+
+        defp set_address(details, %{ address: nil }), do: details
+        defp set_address(details, %{ address: %Address{ street: street, city: city, postcode: postcode, state: state } }), do: Map.put(details, :address, %{ "street" => street, "city" => city, "postcode" => postcode, "state" => state })
+
+        defp set_abn(details, %{ abn: nil }), do: details
+        defp set_abn(details = %{ additional_details: additional_details }, %{ abn: abn }), do: Map.put(details, :additional_details, Map.put(additional_details, "abn", abn))
+        defp set_abn(details, %{ abn: abn }), do: Map.put(details, :additional_details, %{ "abn" => abn })
+
+        defp set_bank(details, %{ bank: nil }), do: details
+        defp set_bank(details = %{ additional_details: additional_details }, %{ bank: %Bank{ bsb: bsb, account_number: account_number } }), do: Map.put(details, :additional_details, Map.put(additional_details, "bank", %{ "bsb" => bsb, "account_number" => account_number }))
+        defp set_bank(details, %{ bank: %Bank{ bsb: bsb, account_number: account_number } }), do: Map.put(details, :additional_details, %{ "bank" => %{ "bsb" => bsb, "account_number" => account_number } })
+    end
 end
