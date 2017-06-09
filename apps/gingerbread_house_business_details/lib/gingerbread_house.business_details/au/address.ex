4fd3f76f57bf304c9043daf950fe298d10d3ebc1
Stefan Johnson <sj@scrimpycat.io>
2017-06-09 11:03:00 +1000

    Add docs for address au

--- apps/gingerbread_house_business_details/lib/gingerbread_house.business_details/au/address.ex
+++ apps/gingerbread_house_business_details/lib/gingerbread_house.business_details/au/address.ex
@@ -1,4 +1,22 @@
 defmodule GingerbreadHouse.BusinessDetails.AU.Address do
+    @moduledoc """
+      A struct representing an Australian address.
+
+      ##Fields
+
+      ###:street
+      Is the street part of the address. Is a `string`.
+
+      ###:city
+      Is the city part of the address. Is a `string`.
+
+      ###:postcode
+      Is the postal code part of the address. Is a `string`.
+
+      ###:state
+      Is the state part of the address. Is a `string`.
+    """
+
     defstruct [
         :street,
         :city,
