8543adb561d7632eaf7a55ddc8d26b48ee0ac793
Stefan Johnson <sj@scrimpycat.io>
2017-06-09 10:58:24 +1000

    Add docs for bank au

--- apps/gingerbread_house_business_details/lib/gingerbread_house.business_details/au/bank.ex
+++ apps/gingerbread_house_business_details/lib/gingerbread_house.business_details/au/bank.ex
@@ -1,4 +1,16 @@
 defmodule GingerbreadHouse.BusinessDetails.AU.Bank do
+    @moduledoc """
+      A struct representing an Australian bank account.
+
+      ##Fields
+
+      ###:bsb
+      Is the BSB of the bank. Is a `string`.
+
+      ###:account_number
+      Is the account number for that bank account. Is a `string`.
+    """
+
     defstruct [
         :bsb,
         :account_number
